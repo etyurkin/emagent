5791caebfc94ae7208ff7eb8ff463708eaea380f
Evgeniy Tyurkin <etyurkin@kwarks.org>
2026-06-28 10:28:00 -0400

    Context, system prompts, and consult/embark integrations

---
+++ lisp/core/emagent-context.el
@@ -0,0 +1,143 @@
+;;; emagent-context.el --- Context injection for emagent -*- lexical-binding: t; -*-
+
+;; Author: Evgeniy Tyurkin <etyurkin@kwarks.org>
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "29.1"))
+
+;;; Code:
+
+(eval-when-compile
+  (require 'cl-lib))
+
+(require 'org)
+(require 'org-element)
+(require 'map)
+(require 'subr-x)
+
+(declare-function flymake-diagnostics "flymake")
+(declare-function flymake-diagnostic-text "flymake")
+(declare-function flymake-diagnostic-type "flymake")
+(declare-function treesit-node-at "treesit")
+(declare-function treesit-node-type "treesit")
+(declare-function treesit-available-p "treesit")
+
+(defun emagent-context--point-info ()
+  "Return point line and column as an alist."
+  (save-excursion
+    (list (cons :line (line-number-at-pos))
+          (cons :column (current-column)))))
+
+(defun emagent-context--region-info ()
+  "Return region bounds and text when active."
+  (when (region-active-p)
+    (list (cons :begin (region-beginning))
+          (cons :end (region-end))
+          (cons :text (buffer-substring-no-properties (region-beginning) (region-end))))))
+
+(defun emagent-context--org-info ()
+  "Return org headline info at point when in org-mode."
+  (when (derived-mode-p 'org-mode)
+    (ignore-errors
+      (let ((element (org-element-at-point)))
+        (when (eq (org-element-type element) 'headline)
+          (list (cons :title (org-element-property :raw-value element))
+                (cons :level (org-element-property :level element))
+                (cons :tags (org-element-property :tags element))))))))
+
+(declare-function which-function "which-func")
+
+(defun emagent-context--which-function ()
+  "Return the enclosing function/method name at point, or nil."
+  (when (fboundp 'which-function)
+    (require 'which-func)
+    (ignore-errors (which-function))))
+
+(defun emagent-context--treesit-node ()
+  "Return the treesit node type at point, or nil."
+  (when (and (fboundp 'treesit-available-p)
+             (treesit-available-p)
+             (fboundp 'treesit-node-at))
+    (ignore-errors
+      (treesit-node-type (treesit-node-at (point))))))
+
+(defun emagent-context--flymake-diagnostics ()
+  "Return flymake diagnostics at point as a list of (TYPE . TEXT) pairs."
+  (when (and (fboundp 'flymake-diagnostics) (bound-and-true-p flymake-mode))
+    (ignore-errors
+      (mapcar (lambda (d)
+                (cons (format "%s" (flymake-diagnostic-type d))
+                      (flymake-diagnostic-text d)))
+              (flymake-diagnostics (point))))))
+
+(defun emagent-context-auto ()
+  "Build automatic Emacs context for the current buffer."
+  (list (cons :buffer (buffer-name))
+        (cons :file (or (buffer-file-name) nil))
+        (cons :major-mode (symbol-name major-mode))
+        (cons :default-directory default-directory)
+        (cons :point (emagent-context--point-info))
+        (cons :enclosing-function (emagent-context--which-function))
+        (cons :treesit-node (emagent-context--treesit-node))
+        (cons :region (emagent-context--region-info))
+        (cons :flymake (emagent-context--flymake-diagnostics))
+        (cons :org (emagent-context--org-info))))
+
+(defun emagent-context-format (context)
+  "Format CONTEXT alist as a readable string block."
+  (let ((lines
+         (list "[Emacs context]"
+               (format "buffer: %s" (map-elt context :buffer))
+               (format "file: %s" (or (map-elt context :file) "<none>"))
+               (format "major-mode: %s" (map-elt context :major-mode))
+               (format "default-directory: %s" (map-elt context :default-directory)))))
+    (when-let* ((point (map-elt context :point)))
+      (setq lines (append lines
+                          (list (format "point: line %s, column %s"
+                                        (map-elt point :line)
+                                        (map-elt point :column))))))
+    (when-let* ((fn (map-elt context :enclosing-function)))
+      (setq lines (append lines (list (format "enclosing-function: %s" fn)))))
+    (when-let* ((node (map-elt context :treesit-node)))
+      (setq lines (append lines (list (format "treesit-node: %s" node)))))
+    (when-let* ((region (map-elt context :region)))
+      (setq lines (append lines
+                          (list (format "region: %s-%s"
+                                        (map-elt region :begin)
+                                        (map-elt region :end))
+                                (format "region-text:\n%s" (map-elt region :text))))))
+    (when-let* ((diags (map-elt context :flymake)))
+      (setq lines (append lines
+                          (list (format "flymake-diagnostics: %s"
+                                        (mapconcat (lambda (d)
+                                                     (format "[%s] %s" (car d) (cdr d)))
+                                                   diags "; "))))))
+    (when-let* ((org (map-elt context :org)))
+      (setq lines (append lines
+                          (list (format "org-headline: level %s, title %s"
+                                        (map-elt org :level)
+                                        (map-elt org :title))))))
+    (string-join lines "\n")))
+
+(defun emagent-context-buffer-summary ()
+  "Return a short summary of the current buffer."
+  (let ((lines (count-lines (point-min) (point-max)))
+        (chars (- (point-max) (point-min))))
+    (format "[Buffer summary]\nname: %s\nlines: %s\nchars: %s\nmode: %s"
+            (buffer-name) lines chars (symbol-name major-mode))))
+
+(defun emagent-context-region ()
+  "Return the active region text or signal an error."
+  (unless (region-active-p)
+    (user-error "No active region"))
+  (format "[Region]\n%s"
+          (buffer-substring-no-properties (region-beginning) (region-end))))
+
+(defun emagent-context-build-prompt (user-text &optional extra-blocks)
+  "Combine USER-TEXT with auto context and EXTRA-BLOCKS."
+  (let* ((auto (emagent-context-format (emagent-context-auto)))
+         (blocks (cons auto (or extra-blocks nil))))
+    (string-join (cons user-text blocks) "\n\n")))
+
+(provide 'emagent-context)
+
+;;; emagent-context.el ends here
---
+++ lisp/integrations/emagent-embark.el
@@ -0,0 +1,71 @@
+;;; emagent-embark.el --- Optional embark integration for emagent -*- lexical-binding: t; -*-
+
+;; Author: Evgeniy Tyurkin <etyurkin@kwarks.org>
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "29.1"))
+
+;;; Commentary:
+;;
+;; Adds an embark target type `emagent-src-block' for #+BEGIN_SRC blocks
+;; inside emagent response regions.  When you call `embark-act' with point
+;; on a src block in an emagent buffer, you get actions to copy the code,
+;; insert it into another buffer, or execute it with org-babel.
+;;
+;; Activated automatically when embark is loaded; no configuration needed.
+
+;;; Code:
+
+(declare-function org-in-src-block-p "org")
+(declare-function org-element-at-point "org-element")
+(declare-function org-element-property "org-element")
+
+(defvar embark-target-finders)
+(defvar embark-keymap-alist)
+
+(defun emagent-embark--src-block-target ()
+  "Embark target for a #+BEGIN_SRC block in an emagent response."
+  (when (and (derived-mode-p 'emagent-mode)
+             (org-in-src-block-p))
+    (let* ((element (org-element-at-point))
+           (value (org-element-property :value element)))
+      (when value
+        (let ((code (string-trim value)))
+          (cons 'emagent-src-block code))))))
+
+(defun emagent-embark-copy-src-block (code)
+  "Copy CODE to the kill ring."
+  (interactive "sCode: ")
+  (kill-new code)
+  (message "emagent: copied src block (%d chars)" (length code)))
+
+(defun emagent-embark-insert-src-block (code)
+  "Insert CODE at point in the selected buffer."
+  (interactive "sCode: ")
+  (let* ((others (seq-filter (lambda (b) (not (eq b (current-buffer))))
+                             (buffer-list)))
+         (target (get-buffer
+                  (completing-read "Insert into buffer: "
+                                   (mapcar #'buffer-name others) nil t))))
+    (with-current-buffer target
+      (insert code))
+    (message "emagent: inserted src block into %s" (buffer-name target))))
+
+(defvar emagent-embark-src-block-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map "c" #'emagent-embark-copy-src-block)
+    (define-key map "i" #'emagent-embark-insert-src-block)
+    (define-key map "e" #'org-babel-execute-src-block)
+    map)
+  "Embark keymap for emagent src blocks.
+Actions: c=copy, i=insert into buffer, e=execute with org-babel.")
+
+(with-eval-after-load 'embark
+  (when (boundp 'embark-general-map)
+    (set-keymap-parent emagent-embark-src-block-map embark-general-map))
+  (add-to-list 'embark-target-finders #'emagent-embark--src-block-target)
+  (add-to-list 'embark-keymap-alist
+               '(emagent-src-block . emagent-embark-src-block-map)))
+
+(provide 'emagent-embark)
+
+;;; emagent-embark.el ends here
