7343a5dd8311bf6dd897fe9471b6613e7a164b1a
Evgeniy Tyurkin <etyurkin@kwarks.org>
2026-07-07 01:54:22 -0400

    Release 1.0.2: allowed-edit diffs and streaming reasoning markdown/whitespace fixes

--- lisp/acp/emagent-acp.el
+++ lisp/acp/emagent-acp.el
@@ -1,7 +1,7 @@
 ;;; emagent-acp.el --- ACP wire-up for emagent -*- lexical-binding: t; -*-
 
 ;; Author: Evgeniy Tyurkin <etyurkin@kwarks.org>
-;; Version: 1.0.1
+;; Version: 1.0.2
 ;; Package-Requires: ((emacs "29.1"))
 
 ;;; Code:
--- lisp/trust/emagent-trust-claude.el
+++ lisp/trust/emagent-trust-claude.el
@@ -1,7 +1,7 @@
 ;;; emagent-trust-claude.el --- Claude Code workspace trust -*- lexical-binding: t; -*-
 
 ;; Author: Evgeniy Tyurkin <etyurkin@kwarks.org>
-;; Version: 1.0.1
+;; Version: 1.0.2
 ;; Package-Requires: ((emacs "29.1"))
 
 ;;; Commentary:
