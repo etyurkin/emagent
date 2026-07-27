73cefdf6dd5172626a0d8519e35f53e4137f1f7b
Evgeniy Tyurkin <etyurkin@users.noreply.github.com>
2026-07-27 07:30:22 -0400

    Bump version to 1.2.8 (#53)

--- emagent-pkg.el
+++ emagent-pkg.el
@@ -2,7 +2,7 @@
 
 ;; Not linked into Elpaca builds; documents the package for tooling.
 
-(define-package "emagent" "1.2.7"
+(define-package "emagent" "1.2.8"
   "Emacs-native ACP chat assistant"
   '((emacs "29.1")))
 
