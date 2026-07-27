48444e710eaa75f715b172d1029e3711dd5b4ec8
Evgeniy Tyurkin <etyurkin@kwarks.org>
2026-07-27 07:29:15 -0400

    Bump version to 1.2.8

--- emagent.el
+++ emagent.el
@@ -6,7 +6,7 @@
 
 ;; Author: Evgeniy Tyurkin <etyurkin@kwarks.org>
 ;; URL: https://github.com/etyurkin/emagent
-;; Version: 1.2.7
+;; Version: 1.2.8
 ;; Package-Requires: ((emacs "29.1"))
 ;; Keywords: comm tools
 
