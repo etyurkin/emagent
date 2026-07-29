50492641d8b9e3d9f7bc85b936f25c69965f8758
Evgeniy Tyurkin <etyurkin@kwarks.org>
2026-07-29 16:23:50 -0400

    Bump version to 1.3.0

--- lisp/tools/emagent-tools.el
+++ lisp/tools/emagent-tools.el
@@ -2,7 +2,7 @@
 
 ;; Author: Evgeniy Tyurkin <etyurkin@kwarks.org>
 ;; SPDX-License-Identifier: MIT
-;; Version: 1.2.8
+;; Version: 1.3.0
 ;; This file is part of emagent.
 ;;
 ;; Permission is hereby granted, free of charge, to any person obtaining a copy
